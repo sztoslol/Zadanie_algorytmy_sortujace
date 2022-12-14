de0715b47680f2686128986b04fb459210447e94
SztosPV <109919302+SztosPV@users.noreply.github.com>
2022-12-14 20:23:08 +0100

    Przypisanie czesci elementów layoutu do zmiennych

--- app/src/main/java/com/example/zadaniealgorytmysortujce/MainActivity.kt
+++ app/src/main/java/com/example/zadaniealgorytmysortujce/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.zadaniealgorytmysortujce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Przypisanie elementów layoutu do zmiennych
+        var wynik_wstawianie = findViewById<TextView>(R.id.textView_wynik_wstawianie)
+        var wynik_babelkowe = findViewById<TextView>(R.id.textView_wynik_babelkowe)
+        var wynik_szybkie = findViewById<TextView>(R.id.textView_wynik_szybkie)
+        var wynik_heap = findViewById<TextView>(R.id.textView_wynik_heap)
+        var wynik_scalanie = findViewById<TextView>(R.id.textView_wynik_scalanie)
 
     }
 }
