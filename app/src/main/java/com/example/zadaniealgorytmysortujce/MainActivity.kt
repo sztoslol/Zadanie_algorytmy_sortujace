355b1738178d7bb24021087620265a2651282359
SztosPV <109919302+SztosPV@users.noreply.github.com>
2022-12-14 20:25:37 +0100

    Przypisanie reszty elementów layoutu do zmiennych

--- app/src/main/java/com/example/zadaniealgorytmysortujce/MainActivity.kt
+++ app/src/main/java/com/example/zadaniealgorytmysortujce/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.zadaniealgorytmysortujce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -9,12 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Przypisanie elementów layoutu do zmiennych
+        //Przypisanie elementów layoutu do zmiennych(TextView)
         var wynik_wstawianie = findViewById<TextView>(R.id.textView_wynik_wstawianie)
         var wynik_babelkowe = findViewById<TextView>(R.id.textView_wynik_babelkowe)
         var wynik_szybkie = findViewById<TextView>(R.id.textView_wynik_szybkie)
         var wynik_heap = findViewById<TextView>(R.id.textView_wynik_heap)
         var wynik_scalanie = findViewById<TextView>(R.id.textView_wynik_scalanie)
 
+        //Przypisanie interaktywnych elementow layoutu do zmiennych
+        var input_ile_razy = findViewById<EditText>(R.id.input_ile_razy)
+        var input_ile_elementow = findViewById<EditText>(R.id.input_ile_elem)
+        var button_main = findViewById<Button>(R.id.button_wykonaj)
     }
 }
