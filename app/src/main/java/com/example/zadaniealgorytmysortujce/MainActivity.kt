a3b968b731fb85a9da3bb060648c3400b4484b5a
SztosPV <109919302+SztosPV@users.noreply.github.com>
2022-12-16 19:36:16 +0100

    Stworzenie funkcji sortowania przez scalanie

--- app/src/main/java/com/example/zadaniealgorytmysortujce/MainActivity.kt
+++ app/src/main/java/com/example/zadaniealgorytmysortujce/MainActivity.kt
@@ -120,3 +120,35 @@ fun heapify(array: IntArray, n: Int, i: Int) {
         heapify(array, n, largest)
     }
 }
+
+//===========================Funkcja sortowania przez scalanie===========================
+fun sortowanie_przez_scalanie(array: IntArray): IntArray {
+    if (array.size <= 1) return array
+
+    val mid = array.size / 2
+    val left = sortowanie_przez_scalanie(array.copyOfRange(0, mid))
+    val right = sortowanie_przez_scalanie(array.copyOfRange(mid, array.size))
+
+    return merge(left, right)
+}
+
+//Funkcja pomocnicza do sortowania przez scalanie
+fun merge(left: IntArray, right: IntArray): IntArray {
+    var leftIndex = 0
+    var rightIndex = 0
+    val result = IntArray(left.size + right.size)
+
+    for (i in result.indices) {
+        if (leftIndex >= left.size) {
+            result[i] = right[rightIndex++]
+        } else if (rightIndex >= right.size) {
+            result[i] = left[leftIndex++]
+        } else if (left[leftIndex] < right[rightIndex]) {
+            result[i] = left[leftIndex++]
+        } else {
+            result[i] = right[rightIndex++]
+        }
+    }
+
+    return result
+}
